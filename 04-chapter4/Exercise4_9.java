0a38db0898e004ee3e4b9befa9aa773f5cbf124f
Nick <nicholasp77@gmail.com>
2015-05-21 23:27:32 -0400

    Add exercise4.9 from chapter 4.

---
+++ 04-chapter4/Exercise4_9.java
@@ -0,0 +1,25 @@
+/*
+This program will recieve a character and display its unicode equivalent
+Nicholas Perez
+*/
+
+import java.util.Scanner;
+
+public class Exercise4_9 {
+	public static void main(String [] args) {
+		
+		//creaitng varaibles for int and char
+		int numChange;
+		char charConsume;
+
+		//Creating Scanner object and prompting user
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a character: ");
+		
+		String entireLine = input.nextLine();
+		charConsume = entireLine.charAt(0);
+
+		//Displaying result with casting
+		System.out.println("The Unicode for the character " + charConsume + " is " + (int)charConsume);
+	}
+}
